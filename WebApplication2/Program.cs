45516213d87293b178fe8051bcf5e0ccdb3cac51
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-11-07 01:39:24 +0530

    initial commit

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -5,16 +5,23 @@ var app = builder.Build();
 
     app.Use(async (context, next) =>
     {
-       await context.Response.WriteAsync("Middle ware 1! ");
-       await next.Invoke();
-       await context.Response.WriteAsync("Middle ware 1 response! ");
+        var key = context.Request.Query["key"];
+        if (!string.IsNullOrWhiteSpace(key))
+        {
+            if (key == "ABC123")
+            {
+              await context.Response.WriteAsync(" You are authorize ");
+            }
+            await next();
+        }
     });
-    app.Use(async (context, next) =>
+
+    /*app.Use(async (context, next) =>
     {
        await context.Response.WriteAsync("Middle ware 2! ");
        await next.Invoke();
        await context.Response.WriteAsync("Middle ware 2 Response! ");
-    });
+    });*/
     app.MapGet("/",async context =>
     {
         await context.Response.WriteAsync("Hello World!");
--- WebApplication2/obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef9d733e8091b37544f655197afcc6e0a89211a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b460e8cb18988ddf8a55892974414f8630b65712")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
