b460e8cb18988ddf8a55892974414f8630b65712
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-11-07 01:24:27 +0530

    initial commit

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -5,16 +5,19 @@ var app = builder.Build();
 
     app.Use(async (context, next) =>
     {
-       await context.Response.WriteAsync("Middle ware! ");
+       await context.Response.WriteAsync("Middle ware 1! ");
        await next.Invoke();
+       await context.Response.WriteAsync("Middle ware 1 response! ");
     });
     app.Use(async (context, next) =>
     {
        await context.Response.WriteAsync("Middle ware 2! ");
        await next.Invoke();
+       await context.Response.WriteAsync("Middle ware 2 Response! ");
     });
     app.MapGet("/",async context =>
     {
         await context.Response.WriteAsync("Hello World!");
+        
     });
     app.Run();
